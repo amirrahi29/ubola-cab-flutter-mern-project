188270e3b5bfa0deba61d01176e83843ffd1bd2a
Amir Rahi <amirrahi29@gmail.com>
2023-07-30 09:21:36 +0530

    added

--- web_app/backend/index.js
+++ web_app/backend/index.js
@@ -1,1 +1,27 @@
-console.log('Helooooo');
+const express = require('express');
+const app = express();
+
+//dotenv
+require('dotenv').config();
+
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://ubola:ps123456@ubolacluster.tfw22hc.mongodb.net/ubola');
+
+mongoose.connection.once('open',()=>{
+    console.log('mongoose connected');
+});
+
+mongoose.connection.on('error',()=>{
+    console.log('Mongoose connection failed '+error);
+});
+
+//api's
+app.use('/',(req,res)=>{
+    res.send('Home page');
+});
+
+//server
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on ${process.env.PORT}`);
+});
